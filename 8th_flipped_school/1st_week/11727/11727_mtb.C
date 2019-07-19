e8f831c9292c913b1a88b7d954ff6d0f1ab8a890
tbmoon <taebong.moon87@gmail.com>
2019-07-20 00:39:05 +0900

    2×n 타일링 2

---
+++ 8th_flipped_school/1st_week/11727/11727_mtb.C
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+const int N = 1000;
+const int mod = 10007;
+int d[N+1];
+int main()
+{
+    int n; cin >> n;
+    d[0] = d[1] = 1;
+    for (int i = 2; i <= n; i++)
+    {   
+        d[i] = d[i-1]+2*d[i-2];
+        d[i] %= mod;
+    }   
+    cout << d[n] << "\n";
+
+    return 0;
+}
